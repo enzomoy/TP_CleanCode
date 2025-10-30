841c946c5f537fca5b958ad1dc92d909f783e61a
Enzo <enzo.moy94@gmail.com>
2025-10-30 10:15:23 +0100

    Add brelan for digit 1

--- src/index.test.ts
+++ src/index.test.ts
@@ -3,8 +3,9 @@ import { rollDices } from './index';
 
 describe('Yams tests', () => {
     it.each([
-        { rools: [[1, 1, 2, 2, 3, 3], [1, 1, 2, 2, 3, 3]], expected: 24 },
-        { rools: [[2, 2, 3, 3, 4, 4], [2, 2, 3, 3, 4, 4]], expected: 36 },
+        { rools: [[1, 1, 2, 2, 3], [1, 1, 2, 2, 3]], expected: 18 }, // Chance
+        { rools: [[2, 2, 3, 3, 4], [2, 2, 3, 3, 4]], expected: 28 }, // Chance
+        { rools: [[1, 1, 1, 2, 3], [1, 1, 1, 2, 3]], expected: 56 }, // Brelan
     ])('should return $expected for rools: $rools', ({ rools, expected }) => {
         expect(rollDices(rools)).toBe(expected);
     });
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,14 @@
 export function rollDices(rools: number[][]): number {
     let total = 0;
     for (const roll of rools) {
+
+        const countOne = roll.filter(val => val === 1).length;
+
+        if (countOne >= 3) {
+            total += 28;
+            continue;
+        }
+
         total += roll.reduce((sum, val) => sum + val, 0);
     }
     return total;
