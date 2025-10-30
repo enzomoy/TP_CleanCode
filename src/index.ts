df28486dca204f988635a123fdfa645e041e512f
Enzo <enzo.moy94@gmail.com>
2025-10-30 10:59:46 +0100

    Add support of full

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,7 @@
 export function rollDices(rools: number[][]): number {
     let total = 0;
     for (const roll of rools) {
-        let countDiceValue: Record<number, number> = {};
-
-        roll.reduce((acc, val) => {
-            acc[val] = (acc[val] || 0) + 1;
-            return acc;
-        }, countDiceValue);
+        let countDiceValue: Record<number, number> = countOccurrences(roll);
 
         // Yams
         if (hasNOfAKind(countDiceValue, 5)) {
@@ -19,6 +14,14 @@ export function rollDices(rools: number[][]): number {
         const isLargeStraight = (uniqueValues.length === 5) && (uniqueValues[4] - uniqueValues[0] === 4);
         if (isLargeStraight) {
             total += 40;
+            continue;
+        }
+
+        // Full
+        const hasThree = hasNOfAKind(countDiceValue, 3);
+        const hasTwo = hasNOfAKind(countDiceValue, 2);
+        if (hasThree && hasTwo) {
+            total += 30;
             continue;
         }
 
@@ -40,12 +43,19 @@ export function rollDices(rools: number[][]): number {
     return total;
 }
 
+function countOccurrences(arr: number[]): Record<number, number> {
+    return arr.reduce((acc, val) => {
+        acc[val] = (acc[val] || 0) + 1;
+        return acc;
+    }, {} as Record<number, number>);
+}
+
 function sumArray(arr: number[]): number {
     return arr.reduce((sum, val) => sum + val, 0);
 }
 
 function hasNOfAKind(counts: Record<number, number>, n: number): boolean {
-    return Object.values(counts).some(count => count >= n);
+    return Object.values(counts).some(count => count == n);
 }
 
 function getUniqueValues(counts: Record<number, number>): number[] {
