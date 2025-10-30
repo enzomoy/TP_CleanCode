45bdaef21bf0577b43af9e9a199b7f00998db31d
Enzo <enzo.moy94@gmail.com>
2025-10-30 10:27:25 +0100

    Add support for Carré

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,19 @@
 export function rollDices(rools: number[][]): number {
     let total = 0;
     for (const roll of rools) {
-
-        // const countOne = roll.filter(val => val === 1).length;
-        // const countTwo = roll.filter(val => val === 2).length;
-        //
-        // if (countOne >= 3 || countTwo >= 3) {
-        //     total += 28;
-        //     continue;
-        // }
-
         let countDiceValue: Record<number, number> = {};
 
         roll.reduce((acc, val) => {
             acc[val] = (acc[val] || 0) + 1;
             return acc;
         }, countDiceValue);
+
+        // Carré
+        const hasFourOfAKind = Object.values(countDiceValue).some(count => count >= 4);
+        if (hasFourOfAKind) {
+            total += 35;
+            continue;
+        }
 
         // Brelan
         const hasThreeOfAKind = Object.values(countDiceValue).some(count => count >= 3);
