9081452a31d9733979ca13541b35edc3a1e468a7
Enzo <enzo.moy94@gmail.com>
2025-10-30 10:23:16 +0100

    Refactor brelan calculation to use reduce

--- src/index.test.ts
+++ src/index.test.ts
@@ -7,6 +7,9 @@ describe('Yams tests', () => {
         { rools: [[2, 2, 3, 3, 4], [2, 2, 3, 3, 4]], expected: 28 }, // Chance
         { rools: [[1, 1, 1, 2, 3], [1, 1, 1, 2, 3]], expected: 56 }, // Brelan of 1
         { rools: [[2, 2, 2, 3, 4], [2, 1, 1, 2, 3]], expected: 37 }, // Brelan of 2
+        { rools: [[3, 3, 3, 1, 2], [2, 1, 1, 2, 4]], expected: 38 }, // Brelan of 3
+        { rools: [[4, 4, 4, 1, 2], [2, 1, 1, 2, 5]], expected: 39 }, // Brelan of 4
+        { rools: [[6, 6, 6, 1, 2], [2, 1, 1, 2, 6]], expected: 40 }, // Brelan of 6
     ])('should return $expected for rools: $rools', ({ rools, expected }) => {
         expect(rollDices(rools)).toBe(expected);
     });
--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,29 @@ export function rollDices(rools: number[][]): number {
     let total = 0;
     for (const roll of rools) {
 
-        const countOne = roll.filter(val => val === 1).length;
-        const countTwo = roll.filter(val => val === 2).length;
+        // const countOne = roll.filter(val => val === 1).length;
+        // const countTwo = roll.filter(val => val === 2).length;
+        //
+        // if (countOne >= 3 || countTwo >= 3) {
+        //     total += 28;
+        //     continue;
+        // }
 
-        if (countOne >= 3 || countTwo >= 3) {
+        let countDiceValue: Record<number, number> = {};
+
+        roll.reduce((acc, val) => {
+            acc[val] = (acc[val] || 0) + 1;
+            return acc;
+        }, countDiceValue);
+
+        // Brelan
+        const hasThreeOfAKind = Object.values(countDiceValue).some(count => count >= 3);
+        if (hasThreeOfAKind) {
             total += 28;
             continue;
         }
 
+        // Chance
         total += roll.reduce((sum, val) => sum + val, 0);
     }
     return total;
